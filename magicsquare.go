02a9349d6553fd959c775dc3569a7b09807f8197
Jason McVetta <jason.mcvetta@gmail.com>
2013-09-19 18:39:00 -0700

    GPLv3

--- magicsquare.go
+++ magicsquare.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+
 package main
 
 import (
